dde93ef188b05fb3dafc7654410633090185855a
Nelio C <114236641+fauteskill@users.noreply.github.com>
2026-05-27 21:22:12 +0100

    Update ex01.py

--- ex01.py
+++ ex01.py
@@ -1,1 +1,8 @@
-print('Hello World')
+import sys
+
+if __name__ == "__main__":     # Read command-line arguments (excluding the script name)     
+  print_args(*sys.argv[1:])
+
+
+idade=30
+print(nome)
